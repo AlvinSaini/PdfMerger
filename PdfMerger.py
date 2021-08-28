d096493966a76840620c3c497e78d653913d424e
alvin saini <codewithalvin@gmail.com>
2021-08-28 10:47:38 +0530

    changes remove classs methods

--- PdfMerger.py
+++ PdfMerger.py
@@ -1,20 +1,23 @@
 import os 
 from PyPDF2 import PdfFileMerger, PdfFileReader 
 
-class PlayWithPdf:	
-	@staticmethod
-	def merge():
-		obj=PdfFileMerger()
-		pdfList=[pdf for pdf in os.listdir() if pdf.endswith(".pdf")]
-		for fil in pdfList:
-			obj.append(PdfFileReader(fil, 'rb')) 
-		obj.write("Merged.pdf") 
+
+def merge(out_name):
+	"""
+	merge the pdfs located in a directory
+	pdfs must be ordered order such as 
+	1.pdf 2.pdf ...
+	a.pdf b.pdf ...
+	"""
+	obj=PdfFileMerger()
+	pdfList=[pdf for pdf in os.listdir() if pdf.endswith(".pdf")]
+	for fil in pdfList:
+		obj.append(PdfFileReader(fil, 'rb')) 
+	obj.write(f"{out_name}.pdf") 
     
 
 if __name__=="__main__":
-	a="/storage/emulated/0/Pdf/"
-	os.chdir(input("Enter path of folder where you have pdfs : "))
-	PlayWithPdf.merge()
+	merge()
 
 
 
